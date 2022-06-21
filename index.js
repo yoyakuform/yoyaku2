bf4975d8057c747bc32552d0bc8bca07e4a7205f
yoyakuform <107911330+yoyakuform@users.noreply.github.com>
2022-06-21 17:37:31 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-   $(function () {
+$(function () {
     // カレンダー
     $(function () {
         $('input[name="date"]').datepicker({
@@ -19,23 +19,15 @@
 
     // 送信
     $('form').submit(function () {
-        var date01 = $('input[name="date01"]').val();
-        var time01 = $('input[name="time01"]').val();
-        var date02 = $('input[name="date02"]').val();
-        var time02 = $('input[name="time02"]').val();
-        var date03 = $('input[name="date03"]').val();
-        var time03 = $('input[name="time03"]').val();
+        var date = $('input[name="date"]').val();
         var number = $('input[name="number"]:checked').val();
         var names = '';
-        var address = $('input[name="address"]').val();
-        var tel = $('input[name="tel"]').val();
-        var comment = $('input[name="comment"]').val();
         $('#form-name').children().each(function (i, elm) {
             names += $(elm).val() + '、';
         })
         names = names.slice(0, -1);
 
-        var msg = `・第一希望日：${date01}\n希望時間：${time01}\n・第二希望日：${date02}\n希望時間：${time02}\n・第三希望日：${date03}\n希望時間：${time03}\n人数：${number}\n氏名：${names}\n住所：${address}\n電話番号：${tel}\n備考：${comment}`;
+        var msg = `希望日：${date}\n人数：${number}\n氏名：${names}`;
         sendText(msg);
 
         return false;
