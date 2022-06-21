92ea4331ce075ed5cde656c6c94c5dea19818531
yoyakuform <107911330+yoyakuform@users.noreply.github.com>
2022-06-21 20:04:08 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+$(function () {
+    // カレンダー
+    $(function () {
+        $('input[name="date01"]').datepicker({
+            dateFormat: 'yy/mm/dd',
+        });
+    });
+  
+  $(function () {
+    // カレンダー
+    $(function () {
+        $('input[name="date02"]').datepicker({
+            dateFormat: 'yy/mm/dd',
+        });
+    });
+    
+    $(function () {
+    // カレンダー
+    $(function () {
+        $('input[name="date03"]').datepicker({
+            dateFormat: 'yy/mm/dd',
+        });
+    });
+
+    // 送信
+    $('form').submit(function () {
+        var date01 = $('input[name="date01"]').val();
+        var date02 = $('input[name="date02"]').val();
+        var date03 = $('input[name="date03"]').val();
+        var number = $('input[name="number"].val();
+        var name = $('input[name="name"].val();
+        var tel = $('input[name="tel"].val();
+        var name = $('input[name="comment"].val();
+
+        var msg = `●第一希望日：${date01}\n●第二希望日：${date02}\n●第三希望日：${date03}\n●人数：${number}\n●氏名：${name}●TEL：${tel}\n●備考：${comment}`;
+        sendText(msg);
+
+        return false;
+    });
+});
