aa963a720a8bf7b191a11b8ed57b516329d438d5
yoyakuform <107911330+yoyakuform@users.noreply.github.com>
2022-06-21 16:21:10 +0900

    Update liff.js

--- liff.js
+++ liff.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     // liffId: LIFF URL "https://liff.line.me/xxx"のxxxに該当する箇所
     // LINE DevelopersのLIFF画面より確認可能
-    var liffId = "1657223848-Xpw65RvQ";
+    var liffId = "1657238503-a4ZR8nPe";
     initializeLiff(liffId);
 })
 
