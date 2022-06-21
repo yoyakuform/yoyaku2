b16567fd61237d102fbb1bd2adc5265894997fdb
yoyakuform <107911330+yoyakuform@users.noreply.github.com>
2022-06-21 20:59:34 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 $(function () {
     // カレンダー
     $(function () {
-        $('input[name="date00"]').datepicker({
+        $('input[name="date"]').datepicker({
             dateFormat: 'yy/mm/dd',
         });
     });
-  
+
+    // 参加人数分の氏名欄を生成
+    $('#form-number').click(function () {
+        $('#form-name').empty();
+        var num = $('input[name="number"]:checked').val();
+        for (i = 0; i < num; i++) {
+            $('#form-name').append(
+                `<input class="form-control w-100 mt-1" name="name" maxlength="10">`
+            );
+        }
+    });
+
     // 送信
     $('form').submit(function () {
-        var date01 = $('input[name="date01"]').val();
-        var date02 = $('input[name="date02"]').val();
-        var date03 = $('input[name="date03"]').val();
-        var number = $('input[name="number"].val();
-        var name = '';
-        var number = $('input[name="tel"].val();
+        var date = $('input[name="date"]').val();
+        var number = $('input[name="number"]:checked').val();
+        var names = '';
+        $('#form-name').children().each(function (i, elm) {
+            names += $(elm).val() + '、';
+        })
+        names = names.slice(0, -1);
 
-        var msg = `第一希望日：${date01}\n第二希望日：${date02}\n第三希望日：${date03}\n人数：${number}\n氏名：${name}\nTEL：${tel}`;
+        var msg = `希望日：${date}\n人数：${number}\n氏名：${names}`;
         sendText(msg);
 
         return false;
