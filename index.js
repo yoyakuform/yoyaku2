5f04e1a66853fc0ad3ee30c9561f16c771589ba3
yoyakuform <107911330+yoyakuform@users.noreply.github.com>
2022-06-21 17:59:20 +0900

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,6 @@
         var time03 = $('input[name="time03"]').val();
         var number = $('input[name="number"]:checked').val();
         var names = '';
-        var address = $('input[name="address"]').val();
         var tel = $('input[name="tel"]').val();
         var text = $('input[name="text"]').val();
         $('#form-name').children().each(function (i, elm) {
@@ -24,7 +23,7 @@
         })
         names = names.slice(0, -1);
 
-        var msg = `・第一希望日：${date01}\n希望時間：${time01}\n・第二希望日：${date02}\n希望時間：${time02}\n・第三希望日：${date03}\n希望時間：${time03}\n人数：${number}\n氏名：${names}\n住所：${address}\n電話番号：${tel}\n備考：${text}`;
+        var msg = `・第一希望日：${date01}\n希望時間：${time01}\n・第二希望日：${date02}\n希望時間：${time02}\n・第三希望日：${date03}\n希望時間：${time03}\n人数：${number}\n氏名：${names}\n電話番号：${tel}\n備考：${text}`;
         sendText(msg);
 
         return false;
