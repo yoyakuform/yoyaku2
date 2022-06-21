cfecd4ee03ed0584a918fdbf0a6cd72cb3a0502d
yoyakuform <107911330+yoyakuform@users.noreply.github.com>
2022-06-21 19:23:47 +0900

    Update index.js

--- index.js
+++ index.js
@@ -6,17 +6,6 @@ $(function () {
         });
     });
 
-    // 参加人数分の氏名欄を生成
-    $('#form-number').click(function () {
-        $('#form-name').empty();
-        var num = $('input[name="number"]:checked').val();
-        for (i = 0; i < num; i++) {
-            $('#form-name').append(
-                `<input class="form-control w-100 mt-1" name="name" maxlength="10">`
-            );
-        }
-    });
-
     // 送信
     $('form').submit(function () {
         var date01 = $('input[name="date01"]').val();
@@ -29,10 +18,6 @@ $(function () {
         var name = $('input[name="name"]').val();
         var tel = $('input[name="tel"]').val();
         var address = $('input[name="address"]').val();
-        $('#form-name').children().each(function (i, elm) {
-            names += $(elm).val() + '、';
-        })
-        names = names.slice(0, -1);
 
         var msg = `・第一希望日：${date01}\n希望時間：${time01}\n・第二希望日：${date02}\n希望時間：${time02}\n・第三希望日：${date03}\n希望時間：${time03}\n人数：${number}\n氏名：${name}\n電話番号：${tel}\n備考：${address}`;
         sendText(msg);
