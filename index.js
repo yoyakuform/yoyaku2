544beae129222f2e8cc39627e36ad8243a5757ee
yoyakuform <107911330+yoyakuform@users.noreply.github.com>
2022-06-21 20:29:11 +0900

    Update index.js

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ $(function () {
         var date = $('input[name="date02"]').val();
         var date = $('input[name="date03"]').val();
         var number = $('input[name="number"].val();
-        var name = $('input[name="name"].val();
+        var date = $('input[name="name"].val();
         var number = $('input[name="tel"].val();
-        var comment = $('input[name="text"].val();
+        var date = $('input[name="text"].val();
 
         var msg = `●第一希望日：${date01}\n●第二希望日：${date02}\n●第三希望日：${date03}\n●人数：${number}\n●氏名：${name}●TEL：${tel}\n●備考：${text}`;
         sendText(msg);
